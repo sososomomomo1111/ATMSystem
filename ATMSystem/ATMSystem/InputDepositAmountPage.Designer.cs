4c7b06a4f84633f66e2217f874224d6f25dc6f75
shota <shota.0126@outlook.jp>
2020-11-12 13:42:37 +0900

    預入金額入力画面のサイズ

--- ATMSystem/ATMSystem/InputDepositAmountPage.Designer.cs
+++ ATMSystem/ATMSystem/InputDepositAmountPage.Designer.cs
@@ -48,10 +48,10 @@
             // confirmButton
             // 
             this.confirmButton.Font = new System.Drawing.Font("MS UI Gothic", 18F);
-            this.confirmButton.Location = new System.Drawing.Point(962, 507);
+            this.confirmButton.Location = new System.Drawing.Point(586, 372);
             this.confirmButton.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
             this.confirmButton.Name = "confirmButton";
-            this.confirmButton.Size = new System.Drawing.Size(323, 132);
+            this.confirmButton.Size = new System.Drawing.Size(200, 65);
             this.confirmButton.TabIndex = 1;
             this.confirmButton.Text = "確認";
             this.confirmButton.UseVisualStyleBackColor = true;
@@ -60,137 +60,139 @@
             // cancelButton
             // 
             this.cancelButton.Font = new System.Drawing.Font("MS UI Gothic", 18F);
-            this.cancelButton.Location = new System.Drawing.Point(57, 507);
+            this.cancelButton.Location = new System.Drawing.Point(14, 372);
             this.cancelButton.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
             this.cancelButton.Name = "cancelButton";
-            this.cancelButton.Size = new System.Drawing.Size(323, 132);
+            this.cancelButton.Size = new System.Drawing.Size(200, 65);
             this.cancelButton.TabIndex = 2;
             this.cancelButton.Text = "キャンセル";
             this.cancelButton.UseVisualStyleBackColor = true;
             // 
             // label1
             // 
-            this.label1.Font = new System.Drawing.Font("MS UI Gothic", 18F);
-            this.label1.Location = new System.Drawing.Point(358, 117);
+            this.label1.Font = new System.Drawing.Font("MS UI Gothic", 15F);
+            this.label1.Location = new System.Drawing.Point(100, 62);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(218, 36);
+            this.label1.Size = new System.Drawing.Size(218, 42);
             this.label1.TabIndex = 3;
             this.label1.Text = "一万円札：";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopRight;
             // 
             // label2
             // 
-            this.label2.Font = new System.Drawing.Font("MS UI Gothic", 18F);
-            this.label2.Location = new System.Drawing.Point(358, 202);
+            this.label2.Font = new System.Drawing.Font("MS UI Gothic", 15F);
+            this.label2.Location = new System.Drawing.Point(100, 114);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(218, 36);
+            this.label2.Size = new System.Drawing.Size(218, 42);
             this.label2.TabIndex = 4;
             this.label2.Text = "五千円札：";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.TopRight;
             // 
             // label3
             // 
-            this.label3.Font = new System.Drawing.Font("MS UI Gothic", 18F);
-            this.label3.Location = new System.Drawing.Point(397, 286);
+            this.label3.Font = new System.Drawing.Font("MS UI Gothic", 15F);
+            this.label3.Location = new System.Drawing.Point(100, 168);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(197, 36);
+            this.label3.Size = new System.Drawing.Size(218, 42);
             this.label3.TabIndex = 5;
             this.label3.Text = "千円札：";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.TopRight;
             // 
             // label4
             // 
-            this.label4.Font = new System.Drawing.Font("MS UI Gothic", 18F);
-            this.label4.Location = new System.Drawing.Point(817, 117);
+            this.label4.Font = new System.Drawing.Font("MS UI Gothic", 15F);
+            this.label4.Location = new System.Drawing.Point(570, 61);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(65, 36);
+            this.label4.Size = new System.Drawing.Size(65, 42);
             this.label4.TabIndex = 6;
             this.label4.Text = "枚";
             // 
             // label5
             // 
-            this.label5.Font = new System.Drawing.Font("MS UI Gothic", 18F);
-            this.label5.Location = new System.Drawing.Point(817, 202);
+            this.label5.Font = new System.Drawing.Font("MS UI Gothic", 15F);
+            this.label5.Location = new System.Drawing.Point(570, 116);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(65, 36);
+            this.label5.Size = new System.Drawing.Size(65, 42);
             this.label5.TabIndex = 7;
             this.label5.Text = "枚";
             // 
             // label6
             // 
-            this.label6.Font = new System.Drawing.Font("MS UI Gothic", 18F);
-            this.label6.Location = new System.Drawing.Point(817, 286);
+            this.label6.Font = new System.Drawing.Font("MS UI Gothic", 15F);
+            this.label6.Location = new System.Drawing.Point(570, 175);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(65, 36);
+            this.label6.Size = new System.Drawing.Size(65, 42);
             this.label6.TabIndex = 8;
             this.label6.Text = "枚";
             // 
             // textBox1
             // 
-            this.textBox1.Font = new System.Drawing.Font("MS UI Gothic", 24F);
-            this.textBox1.Location = new System.Drawing.Point(557, 104);
+            this.textBox1.Font = new System.Drawing.Font("MS UI Gothic", 18F);
+            this.textBox1.Location = new System.Drawing.Point(310, 54);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(246, 55);
+            this.textBox1.Size = new System.Drawing.Size(246, 43);
             this.textBox1.TabIndex = 9;
             // 
             // textBox2
             // 
-            this.textBox2.Font = new System.Drawing.Font("MS UI Gothic", 24F);
-            this.textBox2.Location = new System.Drawing.Point(557, 189);
+            this.textBox2.Font = new System.Drawing.Font("MS UI Gothic", 18F);
+            this.textBox2.Location = new System.Drawing.Point(310, 108);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(246, 55);
+            this.textBox2.Size = new System.Drawing.Size(246, 43);
             this.textBox2.TabIndex = 10;
             // 
             // textBox3
             // 
-            this.textBox3.Font = new System.Drawing.Font("MS UI Gothic", 24F);
-            this.textBox3.Location = new System.Drawing.Point(557, 273);
+            this.textBox3.Font = new System.Drawing.Font("MS UI Gothic", 18F);
+            this.textBox3.Location = new System.Drawing.Point(310, 167);
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(246, 55);
+            this.textBox3.Size = new System.Drawing.Size(246, 43);
             this.textBox3.TabIndex = 11;
             // 
             // label7
             // 
             this.label7.Font = new System.Drawing.Font("MS UI Gothic", 18F);
-            this.label7.Location = new System.Drawing.Point(165, 45);
+            this.label7.Location = new System.Drawing.Point(78, 9);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(956, 36);
+            this.label7.Size = new System.Drawing.Size(597, 42);
             this.label7.TabIndex = 12;
             this.label7.Text = "預け入れる紙幣枚数を入力してください";
             this.label7.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             // 
             // label8
             // 
-            this.label8.Font = new System.Drawing.Font("MS UI Gothic", 12F);
-            this.label8.Location = new System.Drawing.Point(29, 413);
+            this.label8.Font = new System.Drawing.Font("MS UI Gothic", 10F);
+            this.label8.Location = new System.Drawing.Point(10, 270);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(1027, 30);
+            this.label8.Size = new System.Drawing.Size(447, 51);
             this.label8.TabIndex = 13;
             this.label8.Text = "※一回の預け入れ限度枚数は（限度枚数）を超える金額の取り扱いはできません";
             // 
             // label9
             // 
-            this.label9.Font = new System.Drawing.Font("MS UI Gothic", 12F);
-            this.label9.Location = new System.Drawing.Point(29, 453);
+            this.label9.Font = new System.Drawing.Font("MS UI Gothic", 10F);
+            this.label9.Location = new System.Drawing.Point(10, 321);
             this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(928, 35);
+            this.label9.Size = new System.Drawing.Size(391, 41);
             this.label9.TabIndex = 14;
             this.label9.Text = "※お取り扱いは紙幣のみとなっております";
-            this.label9.Click += new System.EventHandler(this.label9_Click);
             // 
             // note
             // 
             this.note.Font = new System.Drawing.Font("MS UI Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
             this.note.ForeColor = System.Drawing.Color.Red;
-            this.note.Location = new System.Drawing.Point(312, 347);
+            this.note.Location = new System.Drawing.Point(164, 217);
             this.note.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
             this.note.Name = "note";
-            this.note.Size = new System.Drawing.Size(602, 56);
+            this.note.Size = new System.Drawing.Size(357, 35);
             this.note.TabIndex = 15;
             this.note.Text = "Note";
             // 
             // InputDepositAmountPage
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1333, 675);
+            //this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.note);
             this.Controls.Add(this.label9);
             this.Controls.Add(this.label8);
